ee445fedd923747e6116d07685e44416117ee237
David Conway <david.l.conway2@gmail.com>
2026-01-05 15:44:02 -0500

    updated naming conventions for posterity.

---
+++ practiceDayOneStringInterpolation/practiceDayOneStringInterpolation/Program.cs
@@ -0,0 +1,10 @@
+﻿string myName = "David Conway";
+int myAge = 37;
+decimal myHeight = 70;
+double myWeight = 245.0;
+
+Console.WriteLine($"Hello! My name is {myName}. I am {myAge} years old. I am {myHeight} inches tall and I weigh {myWeight} lbs.");
+
+
+
+
